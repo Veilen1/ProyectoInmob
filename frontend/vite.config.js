e6e6b38fa09324c6b83d45721ff1b53ebb268a48
Veilen1 <devrientv@gmail.com>
2024-11-23 14:29:37 -0300

    vite fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      template: './public/index.html', // Usa el index.html de la carpeta 'public'
+    })
+  ],
+  publicDir: 'public', // Sirve los archivos estáticos desde aquí
   build: {
     outDir: 'dist',
     rollupOptions: {
@@ -11,5 +19,10 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': '/src' // Alias para facilitar las rutas a src si es necesario
+    }
   }
 });
